bc10fdd7aa39e0eea960932aeb81e70865900d55
Henrique <usuario@MacBook-Pro-de-Usuario.local>
2020-04-12 13:27:35 -0300

    counter answers effect added

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, createRef, useEffect } from "react";
 
 import {
   Container,
@@ -13,41 +13,67 @@ import {
 } from "./styles";
 
 import { Questions, Artists } from "../../data/questions";
+console.log(Questions);
 
-console.log(Questions[0].impressionism.questions);
+// console.log(Questions[0].impressionism.questions);
 
 const quiz = Questions[0].impressionism;
 
 export default function Block({ status }) {
   const [select, setSelect] = useState(null);
+  const [page, setPage] = useState(0);
+  const [rightAnswers, setRightAnswer] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const handleSelect = (key) => {
+  const handleSelect = (key, name) => {
     setSelect(key);
+    if (name.right) {
+      setRightAnswer(rightAnswers + 1);
+    } else {
+      setRightAnswer(0);
+    }
   };
 
-  // useEffect(() => {
-  //   console.log(select);
-  // }, [select]);
+  const handleNextPage = () => {
+    setCounter(rightAnswers + counter);
+    setRightAnswer(0);
+    setSelect(null);
+    setPage(page + 1);
+  };
+
+  const ref = useRef();
 
   return (
     <Container>
       <Box>
         <BoxTop>
-          <Title>{quiz.title} .</Title>
+          <Title>
+            {quiz.title} {counter} .
+          </Title>
         </BoxTop>
         <BoxContent>
-          <Image src={quiz.questions.image} />
+          <Image src={quiz.questions[page].image} />
           <Options>
-            {quiz.questions.options.map((name, key) => (
+            {quiz.questions[page].options.map((name, key) => (
               <Option
+                ref={ref}
                 key={key}
+                isRight={name.right}
                 status={select === key ? 1 : 0}
-                onClick={() => handleSelect(key)}
+                onClick={() => handleSelect(key, name)}
               >
                 {name.name}
               </Option>
             ))}
-            <Button empty={select}>Next</Button>
+            {page <= 2 ? (
+              <Button onClick={() => handleNextPage()} empty={select}>
+                Next
+              </Button>
+            ) : (
+              <Button onClick={() => handleNextPage()} empty={select}>
+                See Answers
+              </Button>
+            )}
           </Options>
         </BoxContent>
       </Box>
